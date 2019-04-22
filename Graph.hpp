2e0881ffad91e91a6e3e603e81d5708e7d294d0d
abenezerarg <abenezerargaw@gmail.com>
2019-04-22 14:39:01 -0600

    fix for issue with malloc

--- Graph.hpp
+++ Graph.hpp
@@ -18,9 +18,9 @@ struct Edge
     vertex *v;
     int distance;
     int duration;
-    PriorityQueue firstClass = PriorityQueue(25);
-    PriorityQueue businessClass = PriorityQueue(50);
-    PriorityQueue coachClass = PriorityQueue(100);
+    PriorityQueue* firstClass = new PriorityQueue(25);
+    PriorityQueue* businessClass = new PriorityQueue(50);
+    PriorityQueue* coachClass = new PriorityQueue(100);
 
 };
 
@@ -43,7 +43,7 @@ class Graph
     void printDFT();
     void printBFT();
     void setAllVerticesUnvisited();
-    void addMember(string name, string class, string depart, string arrive, int bags, int index);
+    void addMember(string name, string class_, string depart, string arrive, int bags, int index);
 
     bool Searchflight(string Depart, string Arrive);
 
